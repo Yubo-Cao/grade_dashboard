3a4d1de9ba2ed04000cb7d71998da43434920a33
Yubo-Cao <cao2006721@gmail.com>
2023-02-18 16:33:06 -0500

    feat: add NoSSR component and test plot

--- view/src/pages/plot.tsx
+++ view/src/pages/plot.tsx
@@ -1,22 +1,21 @@
+import NoSsr from '@/components/NoSsr';
 import React from 'react';
-import Plot from 'react-plotly.js';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Plot
-                data={[
-                    {
-                        x: [1, 2, 3],
-                        y: [2, 6, 3],
-                        type: 'scatter',
-                        mode: 'lines+markers',
-                        marker: { color: 'red' },
-                    },
-                    { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
-                ]}
-                layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
-            />
-        );
-    }
+export default function App() {
+    const Plot = React.lazy(() => import('react-plotly.js'));
+    return (<NoSsr>
+        <Plot
+            data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: { color: 'red' },
+                },
+                { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+            ]}
+            layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+        />
+    </NoSsr>)
 }
